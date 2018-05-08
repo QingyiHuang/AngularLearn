23245ac60682f291f81ee5738b12cb7d7ea0f5d4
QingyiHuang <297947067@qq.com>
2018-05-08 23:32:47 +0800

    event

--- vueevent2/app.js
+++ vueevent2/app.js
@@ -3,6 +3,8 @@ new Vue({
     el:".vue-app",
     data:{
         age:18,
+        x:0,
+        y:0,
     },
     methods:{
         add:function(){
@@ -13,6 +15,16 @@ new Vue({
         },
         rmx:function(num){
             this.age-=num;
+        },
+        updateXY:function(e){//鼠标事件  v-on: 等价于 @
+            this.x=e.offsetX;
+            this.y=e.offsetY;
+        },
+        stopMove:function(e){
+            e.stopPropagation();
+        },
+        alert:function(){
+            alert("hello vue");//to prevent jump
         }
     }
 });
